0b9ef79ac395c782ea336ee562d6406e00ecee60
Samuel <92946129+Cravatec@users.noreply.github.com>
2022-10-23 14:42:40 +0200

    Initial Commit

---
+++ Password_Creator_SwiftUI/Password_Creator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_Creator_SwiftUIApp.swift
+//  Password_Creator_SwiftUI
+//
+//  Created by Sam on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Password_Creator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
